2610bae4979658725a08d730748b84be3fae80f5
Mathieu <bosq.mathieu@gmail.com>
2025-11-04 16:48:07 +0100

    maj

---
+++ country.php
@@ -0,0 +1,49 @@
+<?php
+
+class Country
+{
+    public $name;
+    public $capitale;
+    public $population;
+    public $continent;
+
+    public function __construct($name, $capitale, $population, $continent)
+    {
+        $this->name = $name;
+        $this->capitale = $capitale;
+        $this->population = $population;
+        $this->continent = $continent;
+    }
+
+    public function getInfo()
+    {
+        echo "Le pays {$this->name} se situe en {$this->continent}. Sa capitale est {$this->capitale} et sa population est d’environ {$this->population} millions d’habitants.";
+    }
+
+
+// --- Getters ---
+public function getName() {
+    return $this->name;
+}
+
+public function getCapital() {
+    return $this->capital;
+}
+
+public function getPopulation() {
+    return $this->population;
+}
+
+public function getContinent() {
+    return $this->continent;
+}
+
+// --- Setters ---
+public function setName($name) {
+    $this->name = $name;
+}
+
+public function setPopulation($population) {
+    $this->population = $population;
+}
+}
---
+++ testCountry.php
@@ -0,0 +1,31 @@
+<?php
+// Fichier : test_country.php
+
+require_once 'Country.php'; // Import de la classe
+
+// --- Création d'objets ---
+$france = new Country("France", "Paris", 67, "Europe");
+$japan = new Country("Japon", "Tokyo", 125, "Asie");
+$brazil = new Country("Brésil", "Brasilia", 214, "Amérique du Sud");
+
+// --- Affichage initial ---
+echo "<h2>Avant modification :</h2>";
+echo "<p>" . $france->getInfo() . "</p>";
+echo "<p>" . $japan->getInfo() . "</p>";
+echo "<p>" . $brazil->getInfo() . "</p>";
+
+// --- Modifications avec setters ---
+$france->setName("République Française");
+$japan->setPopulation(126);
+$brazil->setName("DO Brasil");
+
+// --- Affichage après modification ---
+echo "<h2>Après modification :</h2>";
+echo "<p>" . $france->getInfo() . "</p>";
+echo "<p>" . $japan->getInfo() . "</p>";
+echo "<p>" . $brazil->getInfo() . "</p>";
+
+
+
+
+
